b7c6f99272cba0e8905b1b57a22ea2a08faaf7a8
Batin EVIRGEN <batinevirgen@gmail.com>
2020-11-25 10:34:09 +0300

    Moved all the request stuff in to Hashtag class.

--- hashtag.py
+++ hashtag.py
@@ -1,16 +1,41 @@
+import requests
+import constants
+import utils.extract as extract
+
 class Hashtag:
-  def __init__(self, name, mediaCount, topPosts):
-    self.name = name
-    self.mediaCount = mediaCount
-    self.topPosts = topPosts
+  def __init__(self, hashtagName):
+    self.hashtagName = hashtagName
 
     self.minLikes = 1E10
     self.minComments = 1E10
     self.maxLikes = 0
     self.maxComments = 0
 
+  def removeUnnecessaryJSON(self, json):
+    del json["allow_following"]
+    del json["is_following"]
+    del json["is_top_media_only"]
+    del json["profile_pic_url"]
+    del json["edge_hashtag_to_media"]["edges"]
+    del json["edge_hashtag_to_media"]["page_info"]
+    del json["edge_hashtag_to_content_advisory"]
+    del json["edge_hashtag_to_related_tags"]
+    del json["edge_hashtag_to_null_state"]
+    return json
+
+  def extractHashtag(self):
+    r = requests.get(constants.HASHTAG_URL.format(tagName = self.hashtagName))
+    jsonResponse = r.json()
+    jsonResponse = self.removeUnnecessaryJSON(jsonResponse["graphql"]["hashtag"])
+
+    self.name = extract.hashtagName(jsonResponse)
+    self.mediaCount = extract.mediaCount(jsonResponse)
+    self.topPosts = extract.topPosts(jsonResponse)
+
     self.calculateMinMaxOfTopPosts()
     self.calculateEngagement()
+
+    return self
 
   def calculateMinMaxOfTopPosts(self):
     for post in self.topPosts:
@@ -24,5 +49,8 @@ class Hashtag:
       if post.comments > self.maxComments:
         self.maxComments = post.comments
 
+    return self
+
   def calculateEngagement(self):
     self.engagement = ((self.minLikes + 1) / self.mediaCount) * 10
+    return self
--- main.py
+++ main.py
@@ -1,28 +1,9 @@
-import requests
-import constants
 from hashtag import Hashtag
-import utils.extract as extract
 
 # TODO: Check if hashtag is banned or not.
 
-def removeUnnecessaryJSON(json):
-  del json["allow_following"]
-  del json["is_following"]
-  del json["is_top_media_only"]
-  del json["profile_pic_url"]
-  del json["edge_hashtag_to_media"]["edges"]
-  del json["edge_hashtag_to_media"]["page_info"]
-  del json["edge_hashtag_to_content_advisory"]
-  del json["edge_hashtag_to_related_tags"]
-  del json["edge_hashtag_to_null_state"]
+hashtag = Hashtag("fashion").extractHashtag()
 
-  return json
-
-req = requests.get(constants.HASHTAG_URL.format(tagName = "fashion"))
-jsonResponse = req.json()
-jsonResponse = removeUnnecessaryJSON(jsonResponse["graphql"]["hashtag"])
-
-hashtag = Hashtag(extract.hashtagName(jsonResponse), extract.mediaCount(jsonResponse), extract.topPosts(jsonResponse))
 print("Hashtag {0}".format(hashtag.name))
 print("\tMinLikes\t: {0}".format(hashtag.minLikes))
 print("\tMaxLikes\t: {0}".format(hashtag.maxLikes))

e1c643af77c3f2d96f76e1d971b4850797f45de2
Batin EVIRGEN <batinevirgen@gmail.com>
2020-11-26 21:03:08 +0300

    Updated main.py file. It now takes an input and finds related hashtags of that input.

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
-from hashtag import Hashtag
-from user import User
 import hashtagGenerator
+
+def findRelatedHashtags(keywords):
+  relatedHashtags = []
+  for keyword in keywords:
+    related = hashtagGenerator.getRelatedHashtags(keyword)
+    relatedHashtags = relatedHashtags + related
+
+  return relatedHashtags
 
 # TODO: Add related hashtags to search list.
 # In search list, we will check each hashtag in Instagram.
-relatedHashtags = hashtagGenerator.getRelatedHashtags("oslo")
-print(relatedHashtags[5].hashtagName)
+
+hashtagInput = input("Enter sample hashtags for your image (seperated with ','): ")
+
+print("Finding related hashtags.")
+enteredKeywords = hashtagInput.split(',')
+relatedHashtags = findRelatedHashtags(enteredKeywords)
+print("Found {0} hashtags related for your search term.".format(len(relatedHashtags)))
